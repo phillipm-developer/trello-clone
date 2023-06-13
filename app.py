0a6a68d7c26b9d71c4907b06815d79c3ae32d83f
phillipm-developer <14167@coderacademy.edu.au>
2023-06-13 19:31:08 +1000

    Completed register route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 # print(db.__dict__)
 # print(app.config)
@@ -26,7 +28,7 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'email', 'is_admin')
+        fields = ('name', 'email', 'password', 'is_admin')
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -48,17 +50,17 @@ def create_db():
     print('Tables created successfully')
 
 @app.cli.command('seed')
-def seed_db():
+def seed_db(): 
     users = [
         User(
             email = 'admin@spam.com',
-            password = 'spinynorman',
+            password = bcrypt.generate_password_hash('spinynorman').decode('utf8'),
             is_admin = True
         ),
         User(
             name = 'John Cleese',
             email = 'cleese@spam.com',
-            password = 'tisbutascratch'
+            password = bcrypt.generate_password_hash('tisbutascratch').decode('utf8')
         )
     ]
 
@@ -96,6 +98,27 @@ def seed_db():
     # Commit the tranaction to the database
     db.session.commit()
     print('Models seeded')
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Parse, sanitize and validate the incoming JSON data 
+    # via the schema
+    user_info = UserSchema().load(request.json)
+    # Create a new User model instance with the schema data
+    user = User(
+        email = user_info['email'],
+        password = bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+        name = user_info['name']
+    )
+
+    # print(user.__dict__)
+    # Add and commit the new user
+    db.session.add(user)
+    db.session.commit()
+
+    # Return the new user excluding the password
+    return UserSchema(exclude=['password']).dump(user), 201
+
 
 @app.route('/cards')
 def all_cards():
